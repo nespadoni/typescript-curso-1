a0a5e4d16775b756287ba20d337495e8ec64e461
CARLOS BENITO MARTINS SPADONI NETO <137246816+nespadoni@users.noreply.github.com>
2025-04-09 21:36:34 -0400

    Update tsconfig.json: Remove noImplicitAny option and fix formatting

--- app/models/negociacao.ts
+++ app/models/negociacao.ts
@@ -1,16 +1,27 @@
 export class Negociacao {
-    constructor(
-        private _data: Date, 
-        public readonly quantidade: number, 
-        public readonly valor: number
-    ) {}
+    #data
+    #quantidade
+    #valor
 
-    get volume(): number {
-        return this.quantidade * this.valor;
+    constructor(data, quantidade, valor) {
+        this.#data = data;
+        this.#quantidade = quantidade;
+        this.#valor = valor;
     }
 
-    get data(): Date {
-        const data = new Date(this._data.getTime());
-        return data;
+    get data() {
+        return this.#data
+    }
+
+    get quantidade() {
+        return this.#quantidade
+    }
+
+    get valor() {
+        return this.#valor
+    }
+
+    get volume() {
+        return this.#quantidade * this.#valor
     }
 }
--- dist/js/models/negociacao.js
+++ dist/js/models/negociacao.js
@@ -1,14 +1,37 @@
-export class Negociacao {
-    constructor(_data, quantidade, valor) {
-        this._data = _data;
-        this.quantidade = quantidade;
-        this.valor = valor;
+var __classPrivateFieldSet = (this && this.__classPrivateFieldSet) || function (receiver, privateMap, value) {
+    if (!privateMap.has(receiver)) {
+        throw new TypeError("attempted to set private field on non-instance");
     }
-    get volume() {
-        return this.quantidade * this.valor;
+    privateMap.set(receiver, value);
+    return value;
+};
+var __classPrivateFieldGet = (this && this.__classPrivateFieldGet) || function (receiver, privateMap) {
+    if (!privateMap.has(receiver)) {
+        throw new TypeError("attempted to get private field on non-instance");
+    }
+    return privateMap.get(receiver);
+};
+var _data, _quantidade, _valor;
+export class Negociacao {
+    constructor(data, quantidade, valor) {
+        _data.set(this, void 0);
+        _quantidade.set(this, void 0);
+        _valor.set(this, void 0);
+        __classPrivateFieldSet(this, _data, data);
+        __classPrivateFieldSet(this, _quantidade, quantidade);
+        __classPrivateFieldSet(this, _valor, valor);
     }
     get data() {
-        const data = new Date(this._data.getTime());
-        return data;
+        return __classPrivateFieldGet(this, _data);
+    }
+    get quantidade() {
+        return __classPrivateFieldGet(this, _quantidade);
+    }
+    get valor() {
+        return __classPrivateFieldGet(this, _valor);
+    }
+    get volume() {
+        return __classPrivateFieldGet(this, _quantidade) * __classPrivateFieldGet(this, _valor);
     }
 }
+_data = new WeakMap(), _quantidade = new WeakMap(), _valor = new WeakMap();
