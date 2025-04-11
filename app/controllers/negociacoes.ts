69812db932666cfe4b5ea172b7c5cc92b2b549d3
CARLOS BENITO MARTINS SPADONI NETO <137246816+nespadoni@users.noreply.github.com>
2025-04-11 15:08:40 -0400

    chore: clean up unused files and ensure consistency across project structure

--- app/controllers/negociacoes.ts
+++ app/controllers/negociacoes.ts
@@ -2,5 +2,12 @@ import { Negociacao } from "../models/negociacao.js";
 
 export class Negociacoes {
     private negociacoes: Array<Negociacao> = [];
-    
+
+    adiciona(negociacao: Negociacao) {
+        this.negociacoes.push(negociacao);
+    }
+
+    lista(): Array<Negociacao> {
+        return this.negociacoes;
+    }
 }
