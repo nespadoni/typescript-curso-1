4d4ce74b50e2ce78e608e29099bfb3aca5059542
CARLOS BENITO MARTINS SPADONI NETO <137246816+nespadoni@users.noreply.github.com>
2025-04-11 22:20:18 -0400

    refactor: update Negociacao class to improve data retrieval and remove unused getters

--- dist/js/models/negociacao.js
+++ dist/js/models/negociacao.js
@@ -5,15 +5,7 @@ export class Negociacao {
         this._valor = _valor;
     }
     get data() {
-        return this._data;
-    }
-    get quantidade() {
-        return this._quantidade;
-    }
-    get valor() {
-        return this._valor;
-    }
-    get volume() {
-        return this._quantidade * this._valor;
+        const data = new Date(this._data.getTime());
+        return data;
     }
 }
